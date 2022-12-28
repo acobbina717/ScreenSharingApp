32c13ffceb965bdebf4ad00065c4b0557a9ea8a7
Abeku <acobbina717@gmail.com>
2022-12-27 19:31:37 -0500

    .

--- client/src/Components/Modal.tsx
+++ client/src/Components/Modal.tsx
@@ -7,7 +7,7 @@ import {
   UIEvent,
 } from "react";
 import { io } from "socket.io-client";
-const socket = io("https://screen-sharing-app-server.onrender.com");
+const socket = io("http://localhost:4000");
 
 type Props = {
   url: string;
